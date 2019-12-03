50c3391b0e56a055d9643d86ed29b20400b00fd2
MattiasBerlin <mattias-berlin@hotmail.com>
2019-12-03 12:17:28 +0100

    Adjust test to changes

--- day3/day3_test.go
+++ day3/day3_test.go
@@ -3,37 +3,37 @@ package main
 import "testing"
 
 func TestVectorCollidesWith(t *testing.T) {
-	v1 := vector{
-		from: point{0, 0},
-		to:   point{100, 0},
+	v1 := segment{
+		from: point{x: 0, y: 0},
+		to:   point{x: 100, y: 0},
 	}
-	v2 := vector{
-		from: point{50, 50},
-		to:   point{50, -50},
+	v2 := segment{
+		from: point{x: 50, y: 50},
+		to:   point{x: 50, y: -50},
 	}
 	got := v1.collidesWith(v2)
 	if got.x != 50 || got.y != 0 {
 		t.Errorf("%v and %v should intercept at (50, 0), but got %v", v1, v2, got)
 	}
-	v1 = vector{
-		from: point{0, 0},
-		to:   point{100, 0},
+	v1 = segment{
+		from: point{x: 0, y: 0},
+		to:   point{x: 100, y: 0},
 	}
-	v2 = vector{
-		from: point{-10, 50},
-		to:   point{-20, 50},
+	v2 = segment{
+		from: point{x: -10, y: 50},
+		to:   point{x: -20, y: 50},
 	}
 	got = v1.collidesWith(v2)
 	if got.x != 0 || got.y != 0 {
 		t.Errorf("%v and %v should not intercept, but got %v", v1, v2, got)
 	}
-	v1 = vector{
-		from: point{0, 0},
-		to:   point{0, 10},
+	v1 = segment{
+		from: point{x: 0, y: 0},
+		to:   point{x: 0, y: 10},
 	}
-	v2 = vector{
-		from: point{-3, 5},
-		to:   point{3, 5},
+	v2 = segment{
+		from: point{x: -3, y: 5},
+		to:   point{x: 3, y: 5},
 	}
 	got = v1.collidesWith(v2)
 	if got.x != 0 || got.y != 5 {
